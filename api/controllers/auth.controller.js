87e0d25281e899f2926bcc445af1a7e448c486b8
mikeclarencefabregas <20-31529@g.batstate-u.edu.ph>
2024-09-11 15:27:01 +0800

    commi

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,28 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
 export const signup = async (req, res) => {
-  console.log(req.body);
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    !username === "" ||
+    !email === "" ||
+    !password === ""
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    res.json("User registered successfully");
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
