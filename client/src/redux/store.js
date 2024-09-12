56c9f22eb87edf9cba3d915a7783f79d00b7f0ce
mikeclarencefabregas <20-31529@g.batstate-u.edu.ph>
2024-09-12 18:16:30 +0800

    darkmode

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,9 +4,11 @@ import { version } from "mongoose";
 import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
 import { persistStore } from "redux-persist";
+import themeReducer from "./theme/themeSlice";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer,
 });
 
 const persistConfig = {
