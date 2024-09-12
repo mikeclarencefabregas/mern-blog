be852a0afff8ad9295bb4ad51f258cdfda4cbe48
mikeclarencefabregas <20-31529@g.batstate-u.edu.ph>
2024-09-12 10:38:13 +0800

    footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import FooterCom from "./components/Footer";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
+      <FooterCom />
     </BrowserRouter>
   );
 };
