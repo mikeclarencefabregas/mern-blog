67df5aaa9cd1f39031f0ee6e1fb8f9aebd26cac9
mikeclarencefabregas <20-31529@g.batstate-u.edu.ph>
2024-09-17 12:55:14 +0800

    create a post ui

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,55 @@
+import { Button, FileInput, Select, TextInput } from "flowbite-react";
+import React from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const CreatePost = () => {
+  return (
+    <div className="p-3 max-w-3xl mx-auto min-h-screen">
+      <h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
+      <form className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 sm:flex-row justify-between">
+          <TextInput
+            type="text"
+            placeholder="Title"
+            required
+            id="title"
+            className="flex-1"
+          />
+          <Select>
+            <option value="uncategorized">Select a Category</option>
+            <option value="javascript">JavaScript</option>
+            <option value="reactjs">React.js</option>
+            <option value="node">Node</option>
+            <option value="express">Express</option>
+            <option value="mongodb">MongoDB</option>
+            <option value="firebase">Firebase</option>
+            <option value="python">Python</option>
+          </Select>
+        </div>
+        <div className="flex gap-4 items-center justify-between border-4 border-teal-400 border-dashed p-3">
+          <FileInput type="file" accept="image/*" />
+          <Button
+            type="button"
+            gradientDuoTone="purpleToBlue"
+            size="sm"
+            outline
+          >
+            Upload Image
+          </Button>
+        </div>
+        <ReactQuill
+          theme="snow"
+          placeholder="Write something..."
+          className="h-72 mb-12"
+          required
+        />
+      </form>
+      <Button type="submit" gradientDuoTone="purpleToPink" className="w-full">
+        Publish
+      </Button>
+    </div>
+  );
+};
+
+export default CreatePost;
