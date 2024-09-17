48fc46dd828df51e3e1a8b7c92c387f57f5b26ad
mikeclarencefabregas <20-31529@g.batstate-u.edu.ph>
2024-09-17 11:36:36 +0800

    admin function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -46,7 +46,10 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(400, "Invalid Password"));
     }
 
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_Secret);
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_Secret
+    );
 
     const { password: pass, ...rest } = validUser._doc;
     res
@@ -63,7 +66,10 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_Secret);
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_Secret
+      );
       const { password, ...rest } = user._doc;
       res
         .status(200)
@@ -85,7 +91,10 @@ export const google = async (req, res, next) => {
         profilePicture: googlePhotoURL,
       });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_Secret);
+      const token = jwt.sign(
+        { id: newUser._id, isAdmin: newUser.isAdmin },
+        process.env.JWT_Secret
+      );
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -59,7 +59,7 @@ const Header = () => {
       </Button>
       <div className="flex gap-2 md:order-2">
         <Button
-          className="w-12 h-10 hidden sm:inline"
+          className="w-12 h-10 sm:inline"
           color="gray"
           pill
           onClick={() => dispatch(toggleTheme())}
